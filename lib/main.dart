da8dd4674cf0591659f29edaf045671bd5afb8bb
asad <zaman.asadit@gmail.com>
2020-08-10 18:56:17 +0600

    Second commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'network_check.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'হিসাব খাতা',
+      home: CheckNetworkPage(),
+    );
+  }
+}
+
